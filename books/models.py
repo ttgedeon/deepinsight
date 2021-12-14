ff20ea9dffbda2be8faad2456c606a72c79f0041
ttgedeon <tabelagedeon@gmail.com>
2021-12-14 07:31:14 +0100

    start

---
+++ books/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from common.models import Datation
+# Create your models here.
+
+
+class Author(Datation):
+
+    name = models.CharField(max_length=255, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(Datation):
+    title = models.CharField(null=False, blank=False, max_length=255)
+    author = models.ForeignKey(Author, null=False, blank=False, on_delete=models.PROTECT,
+                               related_name='author_books')
